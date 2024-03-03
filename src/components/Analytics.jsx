ececd37d8985af3cdd6e89ee6156dcead2e4595d
Jember <jemberalmojuela08@gmail.com>
2024-03-03 17:21:43 +0800

    initial commit

---
+++ src/components/Analytics.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Laptop from '../assets/laptop.jpg';
+
+const Analytics = () => {
+  return (
+    <div className='w-full bg-white py-16 px-4'>
+      <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
+        <img className='w-[500px] mx-auto my-4' src={Laptop} alt='/' />
+        <div className='flex flex-col justify-center'>
+          <p className='text-[#00df9a] font-bold '>DATA ANALYTICS DASHBOARD</p>
+          <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Manage Data Analytics Centrally</h1>
+          <p>
+                You can configure, view, and observe your data analytics in this dashboard. All the tools you need are already here, 
+                with a user-friendly interface that you will surely love. What are you waiting for?
+          </p>
+          <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3'>Get Started</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Analytics
